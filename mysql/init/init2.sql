7a82700f207025d082f428d721678552cde0b0f6
dyr <511482696@qq.com>
2020-10-29 17:52:57 +0800

    250数据库

--- mysql/init/init2.sql
+++ mysql/init/init2.sql
@@ -18,15 +18,15 @@ truncate table `jobs_info`;
 
 INSERT INTO `admin` VALUES (1, 'admin', 'wyp001@163.com', '{bcrypt}$2a$10$/TFbsEFkPOJi3wnaLUHIZe2DEB0D67mnz14T029XXnnWwyn8.eIwG', 1461978472, 1588164999, '183.11.38.107', '', '', '', 1, 6, 0, 0);
 INSERT INTO `admin` VALUES (2, 'shop', 'wyp001@163.com', '{bcrypt}$2a$10$zfttXeKuxIZu50s9A84GD.Y8dwZb2QxZlwUlg6prJrWOAR9T2x.um', 1461978472, 1588163219, '183.11.38.107', '', '', '', 1, 6, 0, 0);
-INSERT INTO `admin` VALUES (6, 'test', '1377327082@qq.com', '{bcrypt}$2a$10$YCE3gtsoP4KFVKCcNxnfRO3ZfepsBvNspUex5ORaR/nz936ecfoI6', 1521599192, 1522113946, '183.11.39.155', NULL, '', '', 5, 1, 0, 1);
+INSERT INTO `admin` VALUES (3, 'test', '1377327082@qq.com', '{bcrypt}$2a$10$YCE3gtsoP4KFVKCcNxnfRO3ZfepsBvNspUex5ORaR/nz936ecfoI6', 1521599192, 1522113946, '183.11.39.155', NULL, '', '', 5, 1, 0, 1);
 
-INSERT INTO `jobs_info` VALUES (5, NULL, 'jobs-mall', '0 0/1 * * * ?', 'activityJobHandler', NULL, 30, 3, 1597217820000, 1597217880000, 'soubao', '每分钟执行一次', 0, NULL, 1592622709000);
-INSERT INTO `jobs_info` VALUES (6, NULL, 'jobs-mall', '0 0 1 1 * ?', 'mouthJobHandler', NULL, 30, 3, 1596214810000, 1598893200000, 'soubao', '每月1号凌晨1点执行一次', 0, NULL, 1592638707000);
-INSERT INTO `jobs_info` VALUES (7, NULL, 'jobs-order', '0 0 */1 * * ?', 'autoReceiveJob', NULL, 30, 3, 1598259600000, 1611478800000, 'soubao', '每小时执行一次,自动收货确认', 0, NULL, 1592638707000);
-INSERT INTO `jobs_info` VALUES (9, NULL, 'jobs-order', '0 0 0 1-2 * ?', 'autoConfirmJob', NULL, 30, 3, 1596211210000, 1598889600000, 'soubao', '每天执行一次,分销记录自动分成', 0, NULL, 1592638707000);
-INSERT INTO `jobs_info` VALUES (10, NULL, 'jobs-user', '0 0 1 1 * ?', 'mouthJobHandler', NULL, 30, 3, 1596214810000, 1598893200000, 'soubao', '删除一个月以前的短信', 0, NULL, 1592793884000);
-INSERT INTO `jobs_info` VALUES (23, NULL, 'jobs-mall', '1 0 0 * * ?', 'dayJobHandler', NULL, 30, 3, 1597161601000, 1597248001000, 'soubao', '每天0点01秒执行一次', 0, NULL, 1597113303000);
-INSERT INTO `jobs_info` VALUES (24, NULL, 'jobs-mall', '0 0 /1 * * ? ', 'hourJobHandler', NULL, 30, 3, 1597215600000, 1597219200000, 'soubao', '每小时执行一次', 0, NULL, 1597115699000);
+INSERT INTO `jobs_info` VALUES (1, NULL, 'jobs-mall', '0 0/1 * * * ?', 'activityJobHandler', NULL, 30, 3, 1597217820000, 1597217880000, 'soubao', '每分钟执行一次', 0, NULL, 1592622709000);
+INSERT INTO `jobs_info` VALUES (2, NULL, 'jobs-mall', '0 0 1 1 * ?', 'mouthJobHandler', NULL, 30, 3, 1596214810000, 1598893200000, 'soubao', '每月1号凌晨1点执行一次', 0, NULL, 1592638707000);
+INSERT INTO `jobs_info` VALUES (3, NULL, 'jobs-order', '0 0 */1 * * ?', 'autoReceiveJob', NULL, 30, 3, 1598259600000, 1611478800000, 'soubao', '每小时执行一次,自动收货确认', 0, NULL, 1592638707000);
+INSERT INTO `jobs_info` VALUES (4, NULL, 'jobs-order', '0 0 0 1-2 * ?', 'autoConfirmJob', NULL, 30, 3, 1596211210000, 1598889600000, 'soubao', '每天执行一次,分销记录自动分成', 0, NULL, 1592638707000);
+INSERT INTO `jobs_info` VALUES (5, NULL, 'jobs-user', '0 0 1 1 * ?', 'mouthJobHandler', NULL, 30, 3, 1596214810000, 1598893200000, 'soubao', '删除一个月以前的短信', 0, NULL, 1592793884000);
+INSERT INTO `jobs_info` VALUES (6, NULL, 'jobs-mall', '1 0 0 * * ?', 'dayJobHandler', NULL, 30, 3, 1597161601000, 1597248001000, 'soubao', '每天0点01秒执行一次', 0, NULL, 1597113303000);
+INSERT INTO `jobs_info` VALUES (7, NULL, 'jobs-mall', '0 0 /1 * * ? ', 'hourJobHandler', NULL, 30, 3, 1597215600000, 1597219200000, 'soubao', '每小时执行一次', 0, NULL, 1597115699000);
 
 use bb2_mall;
 
